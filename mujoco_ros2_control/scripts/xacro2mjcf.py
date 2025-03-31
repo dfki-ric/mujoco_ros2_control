8401c84f3061bcb1ce0ca9df1e82be208b135bcc
Adrian Danzglock <adrian.danzglock@dfki.de>
2025-03-31 09:20:29 +0200

    renamed LICENSE to LICENCE, replaced licence notes in files with BSD3 and added them to files where it was missing

--- mujoco_ros2_control/scripts/xacro2mjcf.py
+++ mujoco_ros2_control/scripts/xacro2mjcf.py
@@ -13,20 +13,34 @@ import copy
 from urdf2mjcf import create_mjcf_from_urdf
 
 
-## @file xacro2mjcf.py
+# @file xacro2mjcf.py
 # @brief Converts Xacro and URDF files into Mujoco MJCF XML file.
 # @author Adrian Danzglock
 # @date 2023
 #
-# @license GNU General Public License, version 3 (GPL-3.0)
+# @license BSD 3-Clause License
 # @copyright Copyright (c) 2023, DFKI GmbH
 #
-# This file is governed by the GNU General Public License, version 3 (GPL-3.0).
-# The GPL-3.0 is a copyleft license that allows users to use, modify, and distribute software
-# while ensuring that these freedoms are passed on to subsequent users. It requires that any
-#  derivative works or modifications of the software be licensed under the GPL-3.0 as well.
-# You should have received a copy of the GNU General Public License along with this program.
-# If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+# Redistribution and use in source and binary forms, with or without modification, are permitted
+# provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+#    and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+#    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of DFKI GmbH nor the names of its contributors may be used to endorse or promote
+#    products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # This script is a ROS node that converts Xacro and URDF files into a Mujoco MJCF (MuJoCo Composite Format) XML file.
 # It takes a list of input files, including Xacro and URDF files, and a target output file path as parameters. Additionally,
