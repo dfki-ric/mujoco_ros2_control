0f4b7792f21665d3b918f18711c05418cf2ea0e1
Adrian <67166322+Caedael@users.noreply.github.com>
2025-11-21 11:36:19 +0100

    Enhance Docker script for GUI and network setup
    Added Comments

--- run_docker_gui.sh
+++ run_docker_gui.sh
@@ -1,8 +1,11 @@
 #!/bin/env bash
+# Build the container
 docker build -t "mujoco_ros2_control" .
-
+# create the network for the container (it prints a error when the network already exist)
 docker network create ros
+# give permissions to use X11 with docker 
 xhost +local:docker
+# starts the container with the franka example
 docker run \
     --network="ros" \
     --device="/dev/dri:/dev/dri" \
@@ -10,4 +13,5 @@ docker run \
     --volume /tmp/.Xdocker \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -it mujoco_ros2_control bash
+# remove the permissions to use X11 with docker 
 xhost -local:docker
